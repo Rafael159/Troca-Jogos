ff70b50a3546cd319eef533b4e1a33371e11b07e
Rafael Alves <rafael-hyuga@hotmail.com>
2019-06-13 13:14:38 -0300

    Layout Responsive +Página Consoles

--- require/buscaJogo.php
+++ require/buscaJogo.php
@@ -10,29 +10,34 @@
 	$cod = (isset($_POST['cod']) ? $_POST['cod'] : '');
 	$consoles = $console->listarCategorias($cod);
 	
-	foreach ($consoles as $key => $cns) {
+	foreach ($consoles as $key => $cns) :
 		$cons = $cns->nome_console;
-	}
+	endforeach;
 
-	if($way == 'crescente'){		
+	if($way == 'crescente'):		
 		$games = $jogos->listarJogos(array('status'=>'Ativo', 'idconsole'=>$cod, 'order'=>'ORDER BY j.n_jogo')); 
-	}else{
+	else:
 		$games = $jogos->listarJogos(array('status'=>'Ativo', 'idconsole'=>$cod, 'order'=>'ORDER BY j.n_jogo DESC'));		
-	}
+	endif;
 	 
 	$qtd = Jogos::contarJogosHelper(array('status'=>'Ativo', 'idconsole'=>$cod));
-    echo '<label id="info"><span id="nomeConsole">'.strtoupper($cons).'</span><span id="qtdJogo">'.$qtd.'</span> <b>jogo(s) encontrado(s)</b></label><input type="hidden" value="'.$cod.' "id="cod"/>';
-    if($qtd != 0){
-        foreach($games as $valor): ?>
-			<div class="each-game" id="game">
-				<a href="game/game.php?id=<?php echo $valor->id?>&&console=<?php echo $cod;?>">
-				<img src="game/imagens/<?php echo str_replace(' ','',$valor->nome_console)?>/<?php echo $valor->imagem?>" alt="<?php echo strtoupper($valor->n_jogo)?>"/>
-				<span class="nome-jogo"><?php echo strtoupper($valor->n_jogo)?></span>
-				</a>
-          	</div>
-      <?php
-        endforeach;
-	}else{
+    echo '<label id="info"><span id="nomeConsole">'.strtoupper($cons).' - </span><span id="qtdJogo">'.$qtd.'</span> <b>jogo(s) encontrado(s)</b></label><input type="hidden" value="'.$cod.' "id="cod"/>';
+	if($qtd != 0): ?>
+	<div class="row nopadding">
+		<?php
+			foreach($games as $valor): ?>
+				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">								
+					<div class="each-game" id="game">
+						<a href="game/game.php?id=<?php echo $valor->id?>&&console=<?php echo $cod;?>">
+						<img src="game/imagens/<?php echo str_replace(' ','',$valor->nome_console)?>/<?php echo $valor->imagem?>" alt="<?php echo strtoupper($valor->n_jogo)?>"/>
+						<span class="nome-jogo"><?php echo strtoupper($valor->n_jogo)?></span>
+						</a>
+					</div>
+				</div>
+		<?php endforeach; ?>
+	<div> <!-- fim .row -->
+	<?php
+	else:
 		echo "<span id='notFound'>Nada encontrado para ".strtoupper($cons)."</span>";                    
-	}        
+	endif;    
 ?>
--- require/listarCategoria.php
+++ require/listarCategoria.php
@@ -15,44 +15,42 @@
 	 endforeach; 
 ?>	
 
-	<span id="resultado-links">
-		
-			<div class="quadro-galeria" id="<?php echo str_replace(' ', '', $conso)?>">
-				<ul class="galeria-principal">
-					<div class="row nopadding">
-						<?php
-							$games = $jogos->listarJogos(array('idconsole'=>$id, 'status'=>'Ativo', 'limite'=>'4'));
-
-							if(Jogos::contarJogosHelper(array('idconsole'=>$id, 'status'=>'Ativo', 'limite'=>'4')) == 0):
-								if(isset($_SESSION['emailTJ'])):
-									echo "<span class='msgFalha'>Hmmm! Nenhum jogo encontrado para esse console. Seja o primeiro a cadastrar  <a href='users/dashboard.php?secao=jogos'><button id='btn-register'>Cadastrar jogo</button></a></span>";
-								else:
-									echo "<span class='msgFalha'>Hmmm! Nenhum jogo encontrado para esse console. Seja o primeiro a cadastrar <button id='btn-enter' onclick=overlayer('#box-login')>Entrar</button></span>";						
-								endif;
-							else:											
-								foreach($games as $valor):
-						?>
-						<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
-							<li class="vitrine">
-								<a href="game/game.php?codigo=<?php echo $valor->id ?>"><img src="game/imagens/<?php echo str_replace(' ', '', $conso).'/'.$valor->imagem?>" class="img-responsive"/></a>
-								<span class="infogame">
-									<?php
-										$num = strlen($valor->n_jogo);
-										if($num >=22){
-											echo substr($valor->n_jogo,0,22).'...';
-										}else{
-											echo $valor->n_jogo;
-										}
-									?>
-								</span>
-							</li>
-						</div>
+	<span id="resultado-links">		
+		<div class="quadro-galeria" id="<?php echo str_replace(' ', '', $conso)?>">
+			<ul class="galeria-principal">
+				<div class="row nopadding">
 					<?php
-						endforeach;
-					endif;
+						$games = $jogos->listarJogos(array('idconsole'=>$id, 'status'=>'Ativo', 'limite'=>'4'));
+
+						if(Jogos::contarJogosHelper(array('idconsole'=>$id, 'status'=>'Ativo', 'limite'=>'4')) == 0):
+							if(isset($_SESSION['emailTJ'])):
+								echo "<span class='msgFalha'>Hmmm! Nenhum jogo encontrado para esse console. Seja o primeiro a cadastrar  <a href='users/dashboard.php?secao=jogos'><button id='btn-register'>Cadastrar jogo</button></a></span>";
+							else:
+								echo "<span class='msgFalha'>Hmmm! Nenhum jogo encontrado para esse console. Seja o primeiro a cadastrar <button id='btn-enter' onclick=overlayer('#box-login')>Entrar</button></span>";						
+							endif;
+						else:											
+							foreach($games as $valor):
 					?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<li class="vitrine">
+							<a href="game/game.php?codigo=<?php echo $valor->id ?>"><img src="game/imagens/<?php echo str_replace(' ', '', $conso).'/'.$valor->imagem?>" class="img-responsive"/></a>
+							<span class="infogame">
+								<?php
+									$num = strlen($valor->n_jogo);
+									if($num >=22){
+										echo substr($valor->n_jogo,0,22).'...';
+									}else{
+										echo $valor->n_jogo;
+									}
+								?>
+							</span>
+						</li>
 					</div>
-				</ul>
-			
-			</div>
+				<?php
+					endforeach;
+				endif;
+				?>
+				</div>
+			</ul>			
+		</div>
 	</span>
